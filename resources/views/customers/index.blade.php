c06975e4584927d5a4fc52caeb721e40cbba1bdd
ebram101 <144430149+ebram101@users.noreply.github.com>
2024-06-06 13:12:51 +0200

    .

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -59,15 +59,15 @@
                                     {{ $customer->branch->branch_name }}
                                 </td>
                                 <td class="px-4 py-2 md:px-6 md:py-3 text-xs md:text-sm leading-4 text-gray-900 dark:text-gray-100">
+                                   <a href="{{ route('customers.notes', $customer->id) }}" class="text-blue-600 dark:text-blue-400 hover:text-blue-900 dark:hover:text-blue-600">View Notes</a>
                                     <a href="{{ route('strategies.index', $customer) }}" class="text-green-600 dark:text-green-400 hover:text-green-900 dark:hover:text-green-600">View Strategy</a>
-                                    
-                                    <a href="{{ route('customers.edit', $customer) }}" class="text-yellow-600 dark:text-yellow-400 hover:text-yellow-900 dark:hover:text-yellow-600">Edit</a>
-                                    <form action="{{ route('customers.destroy', $customer) }}" method="POST" class="inline" onsubmit="return confirm('Are you sure you want to delete this customer?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-600 dark:text-red-400 hover:text-red-900 dark:hover:text-red-600">Delete</button>
-                                    </form>
-                                </td>
+                                <a href="{{ route('customers.edit', $customer) }}" class="text-yellow-600 dark:text-yellow-400 hover:text-yellow-900 dark:hover:text-yellow-600">Edit</a>
+                                <form action="{{ route('customers.destroy', $customer) }}" method="POST" class="inline" onsubmit="return confirm('Are you sure you want to delete this customer?');">
+                                    @csrf
+                                     @method('DELETE')
+                                       <button type="submit" class="text-red-600 dark:text-red-400 hover:text-red-900 dark:hover:text-red-600">Delete</button>
+                                 </form>
+</td>
                             </tr>
                         @endforeach
                         </tbody>
